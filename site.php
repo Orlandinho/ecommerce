a6721f0998934b71cb41f600539b154c217554c7
Antonio Orlando <horlandomagic@gmail.com>
2019-07-17 21:47:44 -0300

    Alterar Senha

--- site.php
+++ site.php
@@ -583,4 +583,76 @@ $app->get('/profile/orders/:idorder', function($idorder){
 	]);
 });
 
+$app->get('/profile/change-password', function(){
+
+	User::verifyLogin(false);
+
+	$page = new Page();
+
+	$page->setTpl('profile-change-password', [
+		'changePassError'=>User::getError(),
+		'changePassSuccess'=>User::getSuccess()
+	]);
+});
+
+$app->post('/profile/change-password', function(){
+
+	User::verifyLogin(false);
+
+	if (!isset($_POST['current_pass']) || $_POST['current_pass'] === ''){
+
+		User::setError("Digite a senha atual.");
+		header("Location: /profile/change-password");
+		exit;
+	}
+
+	if (!isset($_POST['new_pass']) || $_POST['new_pass'] === ''){
+
+		User::setError("Digite a nova senha.");
+		header("Location: /profile/change-password");
+		exit;
+	}
+
+	if (!isset($_POST['new_pass_confirm']) || $_POST['new_pass_confirm'] === ''){
+
+		User::setError("Confirme a senha.");
+		header("Location: /profile/change-password");
+		exit;
+	}
+
+	if ($_POST['new_pass'] != $_POST['new_pass_confirm']) {
+ 
+		User::setError("A senha de confirmação deve ser igual a nova senha.");
+		header("Location: /profile/change-password");
+		exit;
+ 	}
+
+	if ($_POST['new_pass'] === $_POST['current_pass']){
+
+		User::setError("A senha nova precisa ser diferente da senha atual.");
+		header("Location: /profile/change-password");
+		exit;
+	}
+
+	$user = User::getFromSession();
+
+	if (password_verify($_POST['current_pass'], $user->getdespassword())){
+
+		User::setError("Senha inválida.");
+		header("Location: /profile/change-password");
+		exit;
+	}
+
+	$user->setdespassword($_POST['new_pass']);
+
+	$user->update();
+
+	$_SESSION[User::SESSION] = $user->getValues();//permite que a senha seja alterada novamente depois de uma mudança sem necessidade de deslogar e logar
+
+	User::setSuccess("Senha alterada com sucesso!");
+
+	header("Location: /profile/change-password");
+	exit;
+});
+
 ?>
